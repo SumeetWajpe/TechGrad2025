4c6bdd1520de47f503f76fcdfa2f45b81f4dcd67
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-07-30 13:50:14 +0530

    ✨ Serving contents from Files

--- DAY8_ModuleSystem_Node/Node/BasicServer/app.js
+++ DAY8_ModuleSystem_Node/Node/BasicServer/app.js
@@ -1,12 +1,27 @@
 import { createServer } from "http";
+import fs from "fs";
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+  if (req.url === "/") {
+    fs.readFile("Indexx.html", "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Something went wrong !");
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 Not Found");
+  }
 });
 
 server.listen(port, hostname, () => {
